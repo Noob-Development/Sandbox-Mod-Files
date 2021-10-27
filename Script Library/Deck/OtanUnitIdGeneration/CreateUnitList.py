003490c82ef6926f3dac4e2b47c15fb4d96f4f7e
TheWRDNoob <wargamelearningprogramemail@gmail.com>
2021-10-27 18:59:13 -0400

    Programmatically create XML to hide units

--- Script Library/Deck/OtanUnitIdGeneration/CreateUnitList.py
+++ Script Library/Deck/OtanUnitIdGeneration/CreateUnitList.py
@@ -7,30 +7,32 @@ import GetHiddenUnits
 import pandas as pd
 import json
 
-exclude_list = ["Unit_Su24_Fencer"] # Put units you want to remove here
+exclude_list = [] # Put units you want to remove here
 exclude_list += GetHiddenUnits.get_hidden_units()
 only_1_fob = True
 
 def get_unit_list():
-    final_data = {'num_excluded': 0, 'excluded': exclude_list, 'buildings': [], 'units': []}
+    final_data = {'num_included': 0, 'num_excluded': 0, 'excluded': {'buildings': [], 'units': []}, 'included': {'buildings': [], 'units': []}}
     df = pd.read_csv('TUniteDescriptor.csv')
     for unit in df['ClassNameForDebug']:
         if unit.startswith('Building_'):
             if only_1_fob:
-                if len(final_data['buildings']) == 0:
-                    final_data['buildings'] += [unit]
+                if len(final_data['included']['buildings']) == 0:
+                    final_data['included']['buildings'] += [unit]
                 else:
-                    final_data['num_excluded'] += 1
-                    final_data['excluded'] += [unit]
+                    final_data['excluded']['buildings'] += [unit]
             else:
-                final_data['buildings'] += [unit]
+                final_data['included']['buildings'] += [unit]
             
     df = pd.read_csv('TUniteAuSolDescriptor.csv')
     for unit in df['ClassNameForDebug']:
         if unit not in exclude_list:
-            final_data['units'] += [unit]
+            final_data['included']['units'] += [unit]
         else:
-            final_data['num_excluded'] += 1
+            final_data['excluded']['units'] += [unit]
+
+    final_data['num_excluded'] = len(final_data['excluded']['buildings']) + len(final_data['excluded']['units'])
+    final_data['num_included'] = len(final_data['included']['buildings']) + len(final_data['included']['units'])
     return final_data
 
 if __name__ == '__main__':
--- Script Library/Deck/OtanUnitIdGeneration/GenerateOtanUnitIdsXml.py
+++ Script Library/Deck/OtanUnitIdGeneration/GenerateOtanUnitIdsXml.py
@@ -4,11 +4,11 @@ This is a simple Python script to help write the OtanUnitIds xml file. It expect
 
 import CreateUnitList
 
-total_string = ''
+total_included_string = ''
 unit_list = CreateUnitList.get_unit_list()
 
-for idx, building in enumerate(unit_list['buildings']):
-    total_string += f"""\
+for idx, building in enumerate(unit_list['included']['buildings']):
+    total_included_string += f"""\
         <change property="OtanUnitIds" operation="append" type="map">
             <map>
                 <key type="UInt32">{idx+1}</key>
@@ -22,8 +22,8 @@ for idx, building in enumerate(unit_list['buildings']):
             </map>
         </change>\n"""
         
-for idx, unit in enumerate(unit_list['units'], len(unit_list['buildings'])):
-    total_string += f"""\
+for idx, unit in enumerate(unit_list['included']['units'], len(unit_list['included']['buildings'])):
+    total_included_string += f"""\
             <change property="OtanUnitIds" operation="append" type="map">
                 <map>
                     <key type="UInt32">{idx+1}</key>
@@ -38,4 +38,32 @@ for idx, unit in enumerate(unit_list['units'], len(unit_list['buildings'])):
             </change>\n"""
 
 with open('OtanUnitIdChanges.txt', 'w+') as out:
-    out.write(total_string)
+    out.write(total_included_string)
+
+
+
+total_excluded_string = ''
+
+for building in unit_list['excluded']['buildings']:
+    total_excluded_string += f'''
+    <ndfpatch ndf="pc\\ndf\\patchable\\gfx\\everything.ndfbin" table="TUniteDescriptor" name="Remove {building}">
+        <matchconditions>
+			<matchcondition type="property" property="ClassNameForDebug">{building}</matchcondition>
+		</matchconditions>
+		<changes>
+			<change operation="set" property="ShowInMenu" key="0" type="Boolean">False</change>
+		</changes>
+	</ndfpatch>'''
+for unit in unit_list['excluded']['units']:
+    total_excluded_string += f'''
+    <ndfpatch ndf="pc\\ndf\\patchable\\gfx\\everything.ndfbin" table="TUniteAuSolDescriptor" name="Remove {unit}">
+        <matchconditions>
+			<matchcondition type="property" property="ClassNameForDebug">{unit}</matchcondition>
+		</matchconditions>
+		<changes>
+			<change operation="set" property="ShowInMenu" key="0" type="Boolean">False</change>
+		</changes>
+	</ndfpatch>'''
+
+with open('HideUnits.xml', 'w+') as out:
+    out.write(total_excluded_string)
